330f85d7ab94bdc90787362386ba0c817a4835d8
yuricca <yuricca531@gmail.com>
2021-10-10 22:19:13 +0900

    様々な条件指定の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,8 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    set_product_column
+    set_category_column
   end
 
   def search
@@ -14,6 +16,16 @@ class ProductsController < ApplicationController
 
   def search_product
     @p = Product.ransack(params[:q])
+  end
+
+  def set_product_column
+    @product_name = Product.select("name").distinct
+    @product_size = Product.select("size").distinct
+    @product_status = Product.select("status").distinct
+  end
+
+  def set_category_column
+    @category_name = Category.select("name").distinct
   end
 
 end
